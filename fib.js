26b652d1e7e316752753983a247976d2e3369a80
nickguerra <nickg@maliciousgames.com>
2021-12-29 06:40:05 -0600

    Added memo check in fib function

--- fib.js
+++ fib.js
@@ -1,10 +1,14 @@
-const fib = (n) => {
-    if (n <= 2) return 1;
+// memoization (store duplicate sub problems)
+// js object, keys will be arg to fn, value will be return value
 
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
     return fib(n - 1) + fib(n - 2);
 };
 
 console.log(fib(6)); // 8
-console.log(fib(7)); // 13
-console.log(fib(8)); // 21
-console.log(fib(50)); // 12586269025
+// console.log(fib(7)); // 13
+// console.log(fib(8)); // 21
+// console.log(fib(50)); // 12586269025
