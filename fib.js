b606f0a59275e923f085d30e5bc422e8b749eed1
nickguerra <nickg@maliciousgames.com>
2021-12-29 06:34:50 -0600

    Initial commit

---
+++ fib.js
@@ -0,0 +1,10 @@
+const fib = (n) => {
+    if (n <= 2) return 1;
+
+    return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(6)); // 8
+console.log(fib(7)); // 13
+console.log(fib(8)); // 21
+console.log(fib(50)); // 12586269025
