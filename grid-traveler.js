6bff2b7b7b50294f9eef9dee50b8543c45b9fd5d
nickguerra <nickg@maliciousgames.com>
2021-12-30 14:36:35 -0600

    Added notes for grid traveler problem

---
+++ grid-traveler.js
@@ -0,0 +1,9 @@
+const gridTraveler = (m, n) => {
+
+};
+
+console.log(gridTraveler(1,1)); // 1
+console.log(gridTraveler(2,3)); // 3
+console.log(gridTraveler(3,2)); // 3
+console.log(gridTraveler(3,3)); // 6
+console.log(gridTraveler(18,18)); // 2333606220
