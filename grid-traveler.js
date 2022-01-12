d273e6923377f8c1462734759850112e20605247
nickguerra <nickg@maliciousgames.com>
2022-01-12 05:59:44 -0600

    Added optimized solution for grid traveler problem using memoization

--- grid-traveler.js
+++ grid-traveler.js
@@ -1,8 +1,13 @@
-const gridTraveler = (m, n) => {
+const gridTraveler = (m, n, memo={}) => {
+    const key = m + ',' + n;
+
+    // are the args in the memo?
+    if (key in memo) return memo[key];
     if (m === 1 && n === 1) return 1;
     if (m === 0 || n === 0) return 0;
 
-    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+    memo[key] =  gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+    return memo[key];
 };
 
 console.log(gridTraveler(1,1)); // 1
