2046c295a73911c50d3d4a971981dfd370ec6f04
nickguerra <nickg@maliciousgames.com>
2022-01-16 08:35:10 -0600

    Added stub for can sum problem

---
+++ can-sum.js
@@ -0,0 +1,9 @@
+const canSum = (targetSum, numbers) => {
+
+};
+
+console.log(canSum(7, [2,3])); // true
+console.log(canSum(7, [5, 3, 4, 7])); // true
+console.log(canSum(7, [2,4])); // false
+console.log(canSum(8, [2,3,5])); // true
+console.log(canSum(300, [7,14])); // false
