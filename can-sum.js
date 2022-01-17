401269d75d8883c64f17e0abc8f9512a1f25dfe3
nickguerra <nickg@maliciousgames.com>
2022-01-17 12:10:40 -0600

    Memoized can sum problem

--- can-sum.js
+++ can-sum.js
@@ -1,5 +1,17 @@
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo={}) => {
+if (targetSum in memo) return memo[targetSum];
+ if (targetSum === 0) return true;
+ if (targetSum < 0 ) return false;
 
+ for (let num of numbers) {
+     const remainder = targetSum - num;
+    if(canSum(remainder, numbers, memo) === true) {
+        memo[targetSum] = true;
+        return true;
+    }
+ }
+memo[targetSum] = false;
+ return false;
 };
 
 console.log(canSum(7, [2,3])); // true
