f6a84710230f75ec628d31bf5d3b2fd8c46f1d8c
nickguerra <nickg@maliciousgames.com>
2022-01-21 06:14:59 -0600

    memoized version of howSum method created

--- how-sum.js
+++ how-sum.js
@@ -1,15 +1,17 @@
-const howSum = (targetSum, numbers) => {
+const howSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return [];
     if (targetSum < 0) return null;
 
     for (let num of numbers) {
         const remainder = targetSum - num;
-        const remainderResult = howSum(remainder, numbers);
+        const remainderResult = howSum(remainder, numbers, memo);
         if (remainderResult !== null) {
-            return [...remainderResult, num];
+           memo[targetSum] = [...remainderResult, num];
+           return memo[targetSum];
         }
     }
-
+    memo[targetSum] = null;
     return null;
 };
 
