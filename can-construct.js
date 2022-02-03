2ce1381c448068953bde123f6d36a6a396335f31
nickguerra <nickg@maliciousgames.com>
2022-02-03 05:50:43 -0600

    Added memoization to can-construct problem

--- can-construct.js
+++ can-construct.js
@@ -1,17 +1,17 @@
-const canConstruct = (target, wordBank) => {
-if(target === ''){
-    return true;
-};
+const canConstruct = (target, wordBank, memo={}) => {
+if (target in memo) return memo[target];
+if(target === '') return true;
 
 for (let word of wordBank) {
     if (target.indexOf(word) === 0) {
         const suffix = target.slice(word.length);
-        if (canConstruct(suffix, wordBank) === true) {
+        if (canConstruct(suffix, wordBank, memo) === true) {
+            memo[target] = true;
             return true;
         };
     }
 }
-
+memo[target] = false;
 return false;
 
 };
